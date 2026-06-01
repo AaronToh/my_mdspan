9bba29bf7d215513a9476e264553152d440f66a0
AaronToh <aarontywei@gmail.com>
2026-06-01 15:47:26 +0800

    Implement extents

--- include/my_mdspan/extents.hpp
+++ include/my_mdspan/extents.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 #include <cstddef>
 #include <type_traits>
 
@@ -33,21 +34,30 @@ public:
     }
 
     static constexpr rank_type rank_dynamic() noexcept {
-        // TODO
-        return 0;
+        return ((Extents == dynamic_extent) + ... + 0);
     }
 
     // Default constructor
     constexpr extents() noexcept = default;
 
     // Constructor for dynamic extents: extents<int, dynamic_extent, 4>(3)
-    // TODO: figure out how to accept only the dynamic values
-
-    constexpr index_type extent(rank_type i) const noexcept {
-        // TODO
-        (void)i;
-        return 0;
+    template <class... DynamicExtents>
+    constexpr extents(DynamicExtents... vals) noexcept {
+        static_assert(rank_dynamic() == sizeof...(vals));
+        dynamic_vals = { static_cast<index_type>(vals)... };
     }
-};
 
+    // TODO: figure out how to accept only the dynamic values
+    constexpr index_type extent(rank_type i) const noexcept {
+        if (static_exts[i] != dynamic_extent) return static_cast<index_type>(static_exts[i]);
+        size_t dynamic_i = 0;
+        for (size_t j = 0; j < i; j++) {
+            if (static_exts[j] == dynamic_extent) dynamic_i++;
+        }
+        return dynamic_vals[dynamic_i];
+    }
+private:
+    static constexpr std::array<std::size_t, rank()> static_exts = { Extents... };
+    std::array<index_type, rank_dynamic()> dynamic_vals{};
+};
 } // namespace my
