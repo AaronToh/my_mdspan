61ff6df09e06c22e6068bd218e8d563a72e55b93
AaronToh <aarontywei@gmail.com>
2026-06-07 12:03:37 +0800

    Ops

---
+++ include/my_mdspan/ops.hpp
@@ -0,0 +1,133 @@
+#pragma once
+#include "mdspan.hpp"
+
+namespace my {
+
+namespace detail {
+
+template <class A, class B, class C, class IndexArray, std::size_t... Is>
+void add_base(const A& a, const B& b, C& c, IndexArray& idx, std::index_sequence<Is...>) {
+    c(idx[Is]...) = a(idx[Is]...) + b(idx[Is]...);
+}
+
+template <class A, class B, class C, class IndexArray>
+void add_helper(const A& a, const B& b, C& c, IndexArray& idx, size_t depth) {
+    if (depth == C::rank()) {
+        add_base(a, b, c, idx, std::make_index_sequence<C::rank()>{});
+        return;
+    }
+    for (size_t j = 0; j < c.extent(depth); j++) {
+        idx[depth] = j;
+        add_helper(a, b, c, idx, depth + 1);
+    }
+}
+
+template <
+    class In,
+    class Out,
+    class ElementType,
+    class BinaryOp,
+    class IndexArrayIn,
+    class IndexArrayOut,
+    std::size_t... InIs,
+    std::size_t... OutIs
+>
+void reduce_base(
+    const In& in,
+    Out& out,
+    size_t axis,
+    ElementType init,
+    BinaryOp op,
+    IndexArrayIn& idx_in,
+    IndexArrayOut& idx_out,
+    std::index_sequence<InIs...>,
+    std::index_sequence<OutIs...>
+) {
+    ElementType running = init;
+    for (size_t i = 0; i < in.extent(axis); i++) {
+        idx_in[axis] = i;
+        running = op(running, in(idx_in[InIs]...));
+    }
+    out(idx_out[OutIs]...) = running;
+}
+
+template <
+    class In,
+    class Out,
+    class ElementType,
+    class BinaryOp,
+    class IndexArrayIn,
+    class IndexArrayOut
+>
+void reduce_helper(
+    const In& in,
+    Out& out,
+    size_t axis,
+    ElementType init,
+    BinaryOp op,
+    IndexArrayIn& idx_in,
+    IndexArrayOut& idx_out,
+    size_t depth_in,
+    size_t depth_out
+) {
+    if (depth_in == In::rank()) {
+        reduce_base(in, out, axis, init, op, idx_in, idx_out,
+            std::make_index_sequence<In::rank()>{},
+            std::make_index_sequence<Out::rank()>{});
+        return;
+    }
+    if (depth_in == axis) {
+        reduce_helper(in, out, axis, init, op, idx_in, idx_out, depth_in + 1, depth_out);
+        return;
+    }
+    for (size_t j = 0; j < in.extent(depth_in); j++) {
+        idx_in[depth_in] = j;
+        idx_out[depth_out] = j;
+        reduce_helper(in, out, axis, init, op, idx_in, idx_out, depth_in + 1, depth_out + 1);
+    }
+}
+
+} // namespace detail
+
+template <
+    class ElementType,
+    class Extents,
+    class LayoutPolicyA,
+    class LayoutPolicyB,
+    class LayoutPolicyC,
+    class AccessorPolicy
+>
+void add(
+    const mdspan<ElementType, Extents, LayoutPolicyA, AccessorPolicy>& a,
+    const mdspan<ElementType, Extents, LayoutPolicyB, AccessorPolicy>& b,
+    mdspan<ElementType, Extents, LayoutPolicyC, AccessorPolicy>& c
+) {
+    using index_type = typename Extents::index_type;
+    std::array<index_type, Extents::rank()> idx{};
+    detail::add_helper(a, b, c, idx, 0);
+}
+
+template <
+    class ElementType,
+    class ExtentsIn,
+    class ExtentsOut,
+    class LayoutPolicyIn,
+    class LayoutPolicyOut,
+    class AccessorPolicy,
+    class BinaryOp
+>
+void reduce(
+    const mdspan<ElementType, ExtentsIn, LayoutPolicyIn, AccessorPolicy>& in,
+    mdspan<ElementType, ExtentsOut, LayoutPolicyOut, AccessorPolicy>& out,
+    size_t axis,
+    ElementType init,
+    BinaryOp op
+) {
+    static_assert(ExtentsIn::rank() == ExtentsOut::rank() + 1);
+    using index_type = typename ExtentsIn::index_type;
+    std::array<index_type, ExtentsIn::rank()> idx_in{};
+    std::array<index_type, ExtentsOut::rank()> idx_out{};
+    detail::reduce_helper(in, out, axis, init, op, idx_in, idx_out, 0, 0);
+}
+
+} // namespace my
---
+++ tests/test_ops.cpp
@@ -0,0 +1,94 @@
+#include <my_mdspan/ops.hpp>
+#include <cassert>
+
+int main() {
+    // --- 1D add ---
+    {
+        int a[4] = {1, 2, 3, 4};
+        int b[4] = {10, 20, 30, 40};
+        int c[4] = {};
+
+        my::mdspan<int, my::extents<int, 4>> ma(a, my::extents<int, 4>{});
+        my::mdspan<int, my::extents<int, 4>> mb(b, my::extents<int, 4>{});
+        my::mdspan<int, my::extents<int, 4>> mc(c, my::extents<int, 4>{});
+
+        my::add(ma, mb, mc);
+
+        assert(mc(0) == 11);
+        assert(mc(1) == 22);
+        assert(mc(2) == 33);
+        assert(mc(3) == 44);
+    }
+
+    // --- 2D add, row-major ---
+    {
+        int a[6] = {1, 2, 3, 4, 5, 6};
+        int b[6] = {10, 20, 30, 40, 50, 60};
+        int c[6] = {};
+
+        my::mdspan<int, my::extents<int, 2, 3>> ma(a, my::extents<int, 2, 3>{});
+        my::mdspan<int, my::extents<int, 2, 3>> mb(b, my::extents<int, 2, 3>{});
+        my::mdspan<int, my::extents<int, 2, 3>> mc(c, my::extents<int, 2, 3>{});
+
+        my::add(ma, mb, mc);
+
+        assert(mc(0, 0) == 11);
+        assert(mc(0, 1) == 22);
+        assert(mc(0, 2) == 33);
+        assert(mc(1, 0) == 44);
+        assert(mc(1, 1) == 55);
+        assert(mc(1, 2) == 66);
+    }
+
+    {
+        int a[6] = {1, 2, 3, 4, 5, 6};
+        int b[6] = {10, 20, 30, 40, 50, 60};
+        int c[6] = {};   
+
+        my::mdspan<int, my::extents<int, 2, 3>> ma(a, my::extents<int, 2, 3>{});
+        my::mdspan<int, my::extents<int, 2, 3>, my::layout_left> mb(b, my::extents<int, 2, 3>{});
+        my::mdspan<int, my::extents<int, 2, 3>> mc(c, my::extents<int, 2, 3>{});
+
+        my::add(ma, mb, mc);
+
+        assert(mc(0, 0) == 11);
+        assert(mc(0, 1) == 32);
+        assert(mc(0, 2) == 53);
+        assert(mc(1, 0) == 24);
+        assert(mc(1, 1) == 45);
+        assert(mc(1, 2) == 66);
+    }
+
+    // --- reduce axis 0 (sum along rows): 2x3 -> 3 ---
+    // {{1,2,3},{4,5,6}} -> {5,7,9}
+    {
+        int data[6] = {1, 2, 3, 4, 5, 6};
+        int out[3] = {};
+
+        my::mdspan<int, my::extents<int, 2, 3>> in(data, my::extents<int, 2, 3>{});
+        my::mdspan<int, my::extents<int, 3>> result(out, my::extents<int, 3>{});
+
+        my::reduce(in, result, 0, 0, [](int a, int b){ return a + b; });
+
+        assert(result(0) == 5);
+        assert(result(1) == 7);
+        assert(result(2) == 9);
+    }
+
+    // --- reduce axis 1 (sum along cols): 2x3 -> 2 ---
+    // {{1,2,3},{4,5,6}} -> {6,15}
+    {
+        int data[6] = {1, 2, 3, 4, 5, 6};
+        int out[2] = {};
+
+        my::mdspan<int, my::extents<int, 2, 3>> in(data, my::extents<int, 2, 3>{});
+        my::mdspan<int, my::extents<int, 2>> result(out, my::extents<int, 2>{});
+
+        my::reduce(in, result, 1, 0, [](int a, int b){ return a + b; });
+
+        assert(result(0) == 6);
+        assert(result(1) == 15);
+    }
+
+    return 0;
+}
