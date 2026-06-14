6f8371ccb347231a08ffd0d63186849501853d99
AaronToh <aarontywei@gmail.com>
2026-06-14 10:13:58 +0800

    Raw pointer

--- include/my_mdspan/mdarray.hpp
+++ include/my_mdspan/mdarray.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "mdspan.hpp"
-#include <vector>
 
 namespace my {
 
@@ -19,7 +18,51 @@ public:
 
     constexpr mdarray(const extents_type& e)
         : mapping_(mapping_type(e))
-        , storage_(mapping_.required_span_size()) {}
+        , storage_(new ElementType[mapping_.required_span_size()])
+        , size_(mapping_.required_span_size()) {}
+
+    mdarray(const mdarray& other) 
+        : mapping_(other.mapping_)
+        , storage_(new ElementType[other.size_])
+        , size_(other.size_) {
+        for (size_t i = 0; i < size_; i++) {
+            storage_[i] = other.storage_[i];
+        }
+    }
+
+    mdarray(mdarray&& other)
+        : mapping_(other.mapping_)
+        , storage_(other.storage_)
+        , size_(other.size_) {
+        other.storage_ = nullptr;
+    }
+
+    ~mdarray() { delete[] storage_; }
+
+    mdarray& operator=(const mdarray& other) {
+        if (this != &other) {
+            delete[] storage_; // should be new memory (size might be different)
+            mapping_ = other.mapping_;
+            storage_ = new ElementType[other.size_];
+            size_ = other.size_;
+            for (size_t i = 0; i < size_; i++) {
+                storage_[i] = other.storage_[i];
+            }
+        }
+        return *this;
+    }
+
+    // && is an rvalue reference, ensures that not refernced elsewhere in program
+    mdarray& operator=(mdarray&& other) {
+        if (this != &other) {
+            delete[] storage_;
+            mapping_ = other.mapping_;
+            storage_ = other.storage_;
+            size_ = other.size_;
+            other.storage_ = nullptr;
+        }
+        return *this;
+    }
 
     template <class... Indices>
     constexpr ElementType& operator()(Indices... vals) noexcept {
@@ -32,20 +75,21 @@ public:
     }
 
     constexpr mdspan_type view() const noexcept {
-        return mdspan_type(storage_.data(), mapping_);
+        return mdspan_type(storage_, mapping_);
     }
 
     constexpr mdspan_type view() noexcept {
-        return mdspan_type(storage_.data(), mapping_);
+        return mdspan_type(storage_, mapping_);
     }
 
     constexpr const ElementType* data_handle() const noexcept {
-        return storage_.data();
+        return storage_;
     }
 
 private:
     mapping_type mapping_;
-    std::vector<ElementType> storage_;
+    ElementType* storage_;
+    size_t size_;
 };
 
 } // namespace my
